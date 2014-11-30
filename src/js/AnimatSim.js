dceddcb2ac8e8d2e6474edf4fb64be56c0271d24
vietjtnguyen <viet@vietjtnguyen.com>
2014-11-30 11:33:45 -0800

    Auto-save 2014-11-30T11-33-45

--- src/js/AnimatSim.js
+++ src/js/AnimatSim.js
@@ -9,9 +9,11 @@ namespace.EnvironmentVisualization = require('./EnvironmentVisualization');
 namespace.tileBrushes = require('./tileBrushes');
 namespace.MeshGrid = require('./MeshGrid');
 
-// Basically, if we're in a browser/client environment then we want to throw
-// the exports into the global namespace. Otherwise we want to simply export
-// the namespace as our module.
+// Basically, if we're in a browser/client environment then we want to put the
+// exports into the global namespace, which in a browser/client situation is
+// the window object. We can check if we're in a browser/client situation by
+// checking if the window object exists or not. Otherwise we want to simply
+// export the namespace as our module.
 if ( _.isUndefined(window) )
 {
   module.exports = namespace;
