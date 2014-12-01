48043bbbf75d1220191b7a789cb78d6a509cacb7
vietjtnguyen <viet@vietjtnguyen.com>
2014-11-30 22:45:18 -0800

    Auto-save 2014-11-30T22-45-18

--- src/js/Behaviors.js
+++ src/js/Behaviors.js
@@ -1,6 +1,7 @@
 
 function constrainInsideEnvironment(animat, environment, population)
 {
+  // TODO: Double check if width and height should be swapped.
   animat.x = _.clamp(animat.x, 0, environment.width);
   animat.y = _.clamp(animat.y, 0, environment.height);
 }
