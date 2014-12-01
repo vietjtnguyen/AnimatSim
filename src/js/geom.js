a8c7b463426acd8c7ceacab95d90132d27f3296c
vietjtnguyen <viet@vietjtnguyen.com>
2014-11-30 22:50:18 -0800

    Auto-save 2014-11-30T22-50-18

--- src/js/geom.js
+++ src/js/geom.js
@@ -1,26 +1,25 @@
-module.exports = {
+var exports = {};
 
-  cartesianToPolar: function(xy) {
-    return [
-      Math.atan2(xy[1], xy[0]),
-      Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1])
-    ];
-  },
-
-  polarToCartesian: function(rd) {
-    return [
-      Math.cos(rd[1]) * rd[0],
-      Math.sin(rd[1]) * rd[0]
-    ];
-  },
-
-  rotateVec: function(xy, ang)
-  {
-    return [
-      xy[0] * Math.cos(ang) + xy[1] * -Math.sin(ang),
-      xy[0] * Math.sin(ang) + xy[1] * Math.cos(ang)
-    ];
-  }
-
+exports.cartesianToPolar = function(xy) {
+  return [
+    Math.atan2(xy[1], xy[0]),
+    Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1])
+  ];
 };
 
+exports.polarToCartesian = function(rd) {
+  return [
+    Math.cos(rd[1]) * rd[0],
+    Math.sin(rd[1]) * rd[0]
+  ];
+};
+
+exports.rotateVec = function(xy, ang)
+{
+  return [
+    xy[0] * Math.cos(ang) + xy[1] * -Math.sin(ang),
+    xy[0] * Math.sin(ang) + xy[1] * Math.cos(ang)
+  ];
+};
+
+module.exports = exports;
