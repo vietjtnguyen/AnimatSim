53f14ca34e0ad558c95abb0ee4d5490b762980cf
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-21 12:11:20 -0800

    Auto-save 2014-12-21 12-11-20

--- dist/js/PopulationVisualizationTest.js
+++ dist/js/PopulationVisualizationTest.js
@@ -4,7 +4,7 @@ env.terrain.iterateVertices(function(value, arr, x, y) {
 });
 env.terrain.normalizeValues();
 
-var pop = new AnimatSim.Population();
+var pop = new AnimatSim.Population(env);
 pop.initialize(400, function() {
   return new Animat();
 });
