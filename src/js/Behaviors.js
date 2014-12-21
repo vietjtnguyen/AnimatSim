d30a8be24613ed5f03f55eb7049f5b8144d68427
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-20 23:24:50 -0800

    Auto-save

--- src/js/Behaviors.js
+++
@@ -1,9 +0,0 @@
-var _ = require('./util');
-var geom = require('./geom');
-
-function constrainInsideEnvironment(animat, environment, population)
-{
-  // TODO: Double check if width and height should be swapped.
-  animat.x = _.clamp(animat.x, 0, environment.width);
-  animat.y = _.clamp(animat.y, 0, environment.height);
-}
