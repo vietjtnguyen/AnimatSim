6ce91a692d6e72054f0ddcc3cab8417a5909d51c
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-21 11:34:20 -0800

    Auto-save 2014-12-21 11-34-20

--- src/js/animatBrushes.js
+++ src/js/animatBrushes.js
@@ -5,42 +5,42 @@ var colors = require('./colors');
 
 var exports = {};
 
-exports.normal = function(animat)
+exports.energy = function(animat)
 {
   return colors.animatEnergyColorScale(animat.energy);
 };
 
-swimming_only = function(animat)
+exports.swimming = function(animat)
 {
   return colors.animatSwimmingColorScale(animat.swimming);
 };
 
-eating_only = function(animat)
+exports.eating = function(animat)
 {
   return colors.redGreenColorScale(animat.eatAmount / 0.2);
 };
 
-stomach_only = function(animat)
+exports.stomach = function(animat)
 {
   return colors.redGreenColorScale(animat.stomach);
 };
 
-moving_only = function(animat)
+exports.moving = function(animat)
 {
   return colors.redGreenColorScale(animat.moveAmount / 1.5);
 };
 
-vulnerability_only = function(animat)
+exports.vulnerability = function(animat)
 {
   return colors.greenRedColorScale(animat.vulnerability / 0.01);
 };
 
-distfromhist_only = function(animat)
+exports.distfromhist = function(animat)
 {
   return colors.redGreenColorScale(animat.distanceFromHistory / 10.0);
 };
 
-avoidance_only = function(animat)
+exports.avoidance = function(animat)
 {
   return colors.avoidanceColorScale(animat.avoidance);
 };
