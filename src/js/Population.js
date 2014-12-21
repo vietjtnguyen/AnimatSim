35ac8fb983bb47eedc5e2153b4384ff9efa4d28b
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-21 12:38:20 -0800

    Auto-save 2014-12-21 12-38-20

--- src/js/Population.js
+++ src/js/Population.js
@@ -77,83 +77,83 @@ Population.prototype.step = function(environment)
   }
 };
 
-/**
- */
-Population.prototype.serialize = function() {
-  var i;
-  var animat;
-  var animats = [];
-  for( i = 0; i < this.aliveAnimats.length; i += 1 )
-  {
-    animat = this.aliveAnimats[i];
-    animats.push({index: animat.index, genome: animat.brain.toGenome()});
-  }
-  for( i = 0; i < this.deadAnimats.length; i += 1 )
-  {
-    animat = this.deadAnimats[i];
-    animats.push({index: animat.index, genome: animat.brain.toGenome()});
-  }
-  var metaInfo = {
-    animatCounter: this.animatCounter,
-    generations: app.generations,
-  };
-  switch( app.defaultBrain )
-  {
-  case Brain.markI:
-    metaInfo.defaultBrain = 'markI';
-    break;
-  case Brain.markII:
-    metaInfo.defaultBrain = 'markII';
-    break;
-  case Brian.markIIb:
-    metaInfo.defaultBrain = 'markIIb';
-    break;
-  }
-  animats.push(metaInfo);
-  return JSON.stringify(animats, null, '\t');
-};
+// /**
+//  */
+// Population.prototype.serialize = function() {
+//   var i;
+//   var animat;
+//   var animats = [];
+//   for( i = 0; i < this.aliveAnimats.length; i += 1 )
+//   {
+//     animat = this.aliveAnimats[i];
+//     animats.push({index: animat.index, genome: animat.brain.toGenome()});
+//   }
+//   for( i = 0; i < this.deadAnimats.length; i += 1 )
+//   {
+//     animat = this.deadAnimats[i];
+//     animats.push({index: animat.index, genome: animat.brain.toGenome()});
+//   }
+//   var metaInfo = {
+//     animatCounter: this.animatCounter,
+//     generations: app.generations,
+//   };
+//   switch( app.defaultBrain )
+//   {
+//   case Brain.markI:
+//     metaInfo.defaultBrain = 'markI';
+//     break;
+//   case Brain.markII:
+//     metaInfo.defaultBrain = 'markII';
+//     break;
+//   case Brian.markIIb:
+//     metaInfo.defaultBrain = 'markIIb';
+//     break;
+//   }
+//   animats.push(metaInfo);
+//   return JSON.stringify(animats, null, '\t');
+// };
 
-/**
- */
-Population.prototype.unserialize = function(animatData)
-{
-  var i;
-  var dataIndex = 0;
-  for( i = 0; i < this.aliveAnimats.length; i += 1 )
-  {
-    this.aliveAnimats[i].index = animatData[dataIndex].index;
-    this.aliveAnimats[i].brain.fromGenome(animatData[dataIndex].genome);
-    dataIndex += 1;
-  }
-  for( i = 0; i < this.deadAnimats.length; i += 1 )
-  {
-    this.deadAnimats[i].index = animatData[dataIndex].index;
-    this.deadAnimats[i].brain.fromGenome(animatData[dataIndex].genome);
-    dataIndex += 1;
-  }
-  var metaInfo = animatData[dataIndex];
-  dataIndex += 1;
-  this.animatCounter = metaInfo.animatCounter;
-  app.generations = metaInfo.generations;
-  if( metaInfo.defaultBrain !== undefined )
-  {
-    switch( metaInfo.defaultBrain )
-    {
-    case 'markI':
-      app.defaultBrain = Brain.markI;
-      log('Using brain "markI".');
-      break;
-    case 'markII':
-      app.defaultBrain = Brain.markII;
-      log('Using brain "markII".');
-      break;
-    case 'markIIb':
-      app.defaultBrain = Brain.markIIb;
-      log('Using brain "markIIb".');
-      break;
-    }
-  }
-};
+// /**
+//  */
+// Population.prototype.unserialize = function(animatData)
+// {
+//   var i;
+//   var dataIndex = 0;
+//   for( i = 0; i < this.aliveAnimats.length; i += 1 )
+//   {
+//     this.aliveAnimats[i].index = animatData[dataIndex].index;
+//     this.aliveAnimats[i].brain.fromGenome(animatData[dataIndex].genome);
+//     dataIndex += 1;
+//   }
+//   for( i = 0; i < this.deadAnimats.length; i += 1 )
+//   {
+//     this.deadAnimats[i].index = animatData[dataIndex].index;
+//     this.deadAnimats[i].brain.fromGenome(animatData[dataIndex].genome);
+//     dataIndex += 1;
+//   }
+//   var metaInfo = animatData[dataIndex];
+//   dataIndex += 1;
+//   this.animatCounter = metaInfo.animatCounter;
+//   app.generations = metaInfo.generations;
+//   if( metaInfo.defaultBrain !== undefined )
+//   {
+//     switch( metaInfo.defaultBrain )
+//     {
+//     case 'markI':
+//       app.defaultBrain = Brain.markI;
+//       log('Using brain "markI".');
+//       break;
+//     case 'markII':
+//       app.defaultBrain = Brain.markII;
+//       log('Using brain "markII".');
+//       break;
+//     case 'markIIb':
+//       app.defaultBrain = Brain.markIIb;
+//       log('Using brain "markIIb".');
+//       break;
+//     }
+//   }
+// };
 
 module.exports = Population;
 
