c3d04e6da836259bf5821128637de10fe989927d
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-22 00:44:46 -0800

    Auto-save 2014-12-22 00-44-46

--- test/PopulationVisualization.js
+++ test/PopulationVisualization.js
@@ -6,20 +6,18 @@ var expect = chai.expect;
 var _ = require('lodash');
 
 describe('The PopulationVisualization module', function() {
+  var Population = require('../src/js/Population');
   var PopulationVisualization = require('../src/js/PopulationVisualization');
   it('should contain the PopulationVisualization class', function() {
     expect(PopulationVisualization.name).to.equal('PopulationVisualization');
-    var populationVisualization = new PopulationVisualization();
+    var d3SvgGroup = null;
+    var population = new Population();
+    var populationVisualization = new PopulationVisualization(d3SvgGroup, population);
     expect(populationVisualization).to.exist();
   });
 
   describe('The PopulationVisualization class', function() {
-    it('should start off empty');
-    it('should add invidual animats through the add method');
-    it('should populate appropriately using the populate method');
-    it('should move all animats from alive to dead using the killAll method');
-    it('should move all animats from dead to alive and reset their states using the reset method');
-    it('should call step on all alive animats when performing a simulation step');
+    it('should complain if a population is not provided on construction');
   });
 });
 
