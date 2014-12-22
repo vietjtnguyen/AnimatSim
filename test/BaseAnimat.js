1ad4d6fe8afa4f0041e1d80b3c077ac241788c0b
vietjtnguyen <viet@vietjtnguyen.com>
2014-12-21 23:27:45 -0800

    Auto-save 2014-12-21 23-27-45

---
+++ test/BaseAnimat.js
@@ -0,0 +1,52 @@
+var mocha = require('mocha');
+var chai = require('chai');
+
+var expect = chai.expect;
+
+var _ = require('lodash');
+
+describe('The BaseAnimat module', function() {
+  var BaseAnimat = require('../src/js/BaseAnimat');
+  it('should contain the BaseAnimat class', function() {
+    expect(BaseAnimat.name).to.equal('BaseAnimat');
+    var baseAnimat = new BaseAnimat();
+    expect(baseAnimat).to.exist();
+  });
+
+  describe('The BaseAnimat class', function() {
+    it('should construct with sensible defaults', function() {
+      var baseAnimat = new BaseAnimat();
+      expect(baseAnimat.ticks).to.equal(BaseAnimat.defaultSettings.ticks);
+      expect(baseAnimat.x).to.equal(BaseAnimat.defaultSettings.x);
+      expect(baseAnimat.y).to.equal(BaseAnimat.defaultSettings.y);
+      expect(baseAnimat.dir).to.equal(BaseAnimat.defaultSettings.dir);
+      expect(baseAnimat.customReset).to.equal(BaseAnimat.defaultSettings.customReset);
+    });
+    it('should accept settings to override defaults', function() {
+      var settings = {
+        ticks: 1,
+        x: 2,
+        y: 3,
+        dir: 4,
+        customReset: function() { return; }
+      };
+      var baseAnimat = new BaseAnimat(settings);
+      expect(baseAnimat.ticks).to.equal(settings.ticks);
+      expect(baseAnimat.x).to.equal(settings.x);
+      expect(baseAnimat.y).to.equal(settings.y);
+      expect(baseAnimat.dir).to.equal(settings.dir);
+      expect(baseAnimat.customReset).to.equal(settings.customReset);
+    });
+    it('should accept calculate width and height correctly', function() {
+      var settings = {
+        rows: 11,
+        cols: 11,
+        tileSize: 33.0
+      };
+      var baseAnimat = new BaseAnimat(settings);
+      expect(baseAnimat.width).to.equal(settings.rows * settings.tileSize);
+      expect(baseAnimat.height).to.equal(settings.cols * settings.tileSize);
+    });
+  });
+});
+
